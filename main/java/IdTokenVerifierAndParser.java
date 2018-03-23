cf6765f738291f271204c55ce29b3598ee118910
swetha-Full <36259984+swetha-Full@users.noreply.github.com>
2018-03-23 18:06:46 +0530

    first commit

---
+++ main/java/IdTokenVerifierAndParser.java
@@ -0,0 +1,28 @@
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+public class IdTokenVerifierAndParser {
+	private static final String GOOGLE_CLIENT_ID = "116725367766-hf13gjp82j1a0c8mb0lm00doofaq18h1.apps.googleusercontent.com";
+	 public static GoogleIdToken.Payload getPayload (String tokenString) throws Exception {
+
+	        JacksonFactory jacksonFactory = new JacksonFactory();
+	        GoogleIdTokenVerifier googleIdTokenVerifier =
+	                            new GoogleIdTokenVerifier(new NetHttpTransport(), jacksonFactory);
+
+	        GoogleIdToken token = GoogleIdToken.parse(jacksonFactory, tokenString);
+
+	        if (googleIdTokenVerifier.verify(token)) {
+	            GoogleIdToken.Payload payload = token.getPayload();
+	            if (!GOOGLE_CLIENT_ID.equals(payload.getAudience())) {
+	                throw new IllegalArgumentException("Audience mismatch");
+	            } else if (!GOOGLE_CLIENT_ID.equals(payload.getAuthorizedParty())) {
+	                throw new IllegalArgumentException("Client ID mismatch");
+	            }
+	            return payload;
+	        } else {
+	            throw new IllegalArgumentException("id token cannot be verified");
+	        }
+}
+}
---
+++ main/webapp/welcome.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Welcome</title>
+</head>
+<body>
+<% if (session.getAttribute("userName") == null) {%>
+<jsp:forward page="/index.jsp"/>
+<% } %>
+
+<h3>Welcome  ${userName}</h3>
+<a href="index.jsp" onclick="signOut();">SignOut</a>
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');}
+  };
+</script>
+</body>
+</html>
